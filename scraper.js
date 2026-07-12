77140ca5444c42d7876fe0882475980f61ea8799
ahneth <ahneth@gmail.com>
2026-07-12 16:05:03 +0800

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,4 +1,7 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
 const fs = require('fs');
 const path = require('path');
 
